3afbf30e2d3795593b602f49870ec761476fbce4
gerard <gerardacostabazaga@gmail.com>
2024-06-10 23:29:13 +0200

    Creada la lógica mínima para el funcionamiento de la web y restructurada las carpetas

---
+++ src/scripts/scripts.js
@@ -0,0 +1,47 @@
+document.querySelector("#generar").addEventListener("click", function(event){
+    generatePassword(event);
+});
+
+function generateNum(list){
+    return Math.floor(Math.random() * (list.length) + 1);
+}
+
+function checkPasswordCorrect(password){
+    console.log(PATTERN_MAY.test(password));
+    if(checkMayuscula.checked && (PATTERN_MAY.test(password)==false)) return false;
+    if(checkMinuscula.checked && (PATTERN_MIN.test(password)==false)) return false;
+    if(checkNumeros.checked && (PATTERN_NUM.test(password)==false)) return false;
+    if(checkSimbolos.checked && (PATTERN_SYM.test(password)==false)) return false;
+    
+    return true;
+}
+
+function generatePassword(event){
+    event.preventDefault();
+    textPassw.innerText = "";
+    let posibles = [];
+    exit = false;
+
+    if(checkMayuscula.checked) posibles = posibles.concat(ABC_MAY);
+    if(checkMinuscula.checked) posibles = posibles.concat(ABC_MIN);
+    if(checkNumeros.checked) posibles = posibles.concat(NUM); 
+    if(checkSimbolos.checked) posibles = posibles.concat(SYMB);
+
+    while (!exit){
+        password = "";
+        for(i=0; i<=8; i++){
+            num = generateNum(posibles);
+            password = password.concat(posibles[num-1]);
+        };
+
+        exit = checkPasswordCorrect(password);
+        console.log(exit)
+    };
+    console.log("La contraseña es: ", password);
+
+    textPassw.innerText = password;
+} 
+
+function copyPassword(){
+    navigator.clipboard.writeText(textPassw.textContent);
+}
---
+++ src/scripts/variables-globales.js
@@ -0,0 +1,21 @@
+const textPassw = document.getElementById("contraseña-generada");
+textPassw.innerText = "Contraseña generada";
+
+const checkMayuscula = document.getElementById("mayuscula");
+const checkMinuscula = document.getElementById("minuscula");
+const checkNumeros = document.getElementById("numero");
+const checkSimbolos = document.getElementById("simbolo");
+
+const PATTERN_MAY = /(?=.*[A-Z])/;
+const PATTERN_MIN = /(?=.*[a-z])/;
+const PATTERN_NUM = /(?=.*[0-9])/;
+const PATTERN_SYM = /(?=.*[!#%&<>=@{}~$()*+?]+)/;
+
+let posibles = [];
+let password = "";
+let exit = false;
+
+const ABC_MIN = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const ABC_MAY = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const NUM = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const SYMB = ["$", "@", "#", "&", "!", "¡", "?", "¿"];
