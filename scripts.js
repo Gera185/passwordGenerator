ea6b0b1f14d07e52b559d3a8046581d8cce74282
gerard <gerardacostabazaga@gmail.com>
2024-06-10 18:58:17 +0200

    Cambios ortográficos y creado el .gitignore

--- scripts.js
+++ scripts.js
@@ -9,15 +9,15 @@ let password = "";
 
 const ABC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const NUM = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const SYMB = ["$", "@"];
+const SYMB = ["$", "@", "#", "&", "!", "¡", "?", "¿"];
 
 document.querySelector("#generar").addEventListener("click", function(event){
     event.preventDefault();
     if(checkMayuscula.checked){
         console.log("Estoy checkeado")
     };
-    console.log("Holaa funciono");
-    textPassw.innerText = "Holaa funciono";
+    console.log("Hola funciono");
+    textPassw.innerText = "Hola funciono";
 });
 
 
